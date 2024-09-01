f564d94d277f0511d1b9fbef72b79c934f12f677
Add9 <add9@add9.dev>
2024-09-01 18:38:38 +0200

    fixed typo + license

--- src/components/DownloadICSSection/DownloadICSSection.jsx
+++ src/components/DownloadICSSection/DownloadICSSection.jsx
@@ -5,7 +5,7 @@ const DownloadICSSection = ({ icsLink, isDisabled }) => {
     return (
         <div className="download-button-container">
             <a href={icsLink} download className={`download-button ${isDisabled ? 'disabled' : ''}`} onClick={(e) => isDisabled && e.preventDefault()}>
-                Télécharger le fichier .ics pour l'importer sur votre calendrier
+                Télécharger le fichier .ics pour l'importer sur un calendrier externe
             </a>
         </div>
     );
