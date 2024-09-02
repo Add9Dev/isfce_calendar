2917e0551698ed2af39a0bf0a06f46bde741a89c
Add9 <add9@add9.dev>
2024-09-02 13:09:09 +0200

    Course Planner

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,12 +6,14 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-links">
+                <a href="/" className="footer-link">Voir les horaires</a>
                 <a href="/course-planner" className="footer-link">Créer un horaire personnalisé</a>
-                <a href="https://github.com/Add9Dev/isfce_calendar/" target="_blank" rel="noopener noreferrer" className="footer-link">
+                <a href="https://github.com/Add9Dev/isfce_calendar/" target="_blank" rel="noopener noreferrer"
+                   className="footer-link">
                     Voir sur GitHub
                 </a>
                 <a href="https://isfce.org/" target="_blank" rel="noopener noreferrer" className="footer-link">
-                    Aller sur le site de  l'ISFCE
+                    Aller sur le site de l'ISFCE
                 </a>
             </div>
         </footer>
